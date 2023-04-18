185261d628b9fa6610e88f8d00eea080c1d73d79
gezelligheld <1970173678@qq.com>
2023-04-18 18:12:11 +0800

    fix: 导出

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
+export { default as useTable } from './useTable';
 export { default as useUnmount } from './useUnmount';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,14 @@
 export { default as arrayBufferToBase64 } from './arrayBufferToBase64';
 export { default as base64ToFile } from './base64ToFile';
 export { default as download } from './download';
+export { default as drawImage } from './drawImage';
+export { default as getQueryParam } from './getQueryParam';
+export { default as getRandomString } from './getRandomString';
+export { default as getSuffixName } from './getSuffixName';
 export { default as insertText } from './insertText';
+export { default as int8ToInt16 } from './int8ToInt16';
+export { default as isBrowser } from './isBrowser';
+export { default as isMobile } from './isMobile';
+export { default as isSupportWebp } from './isSupportWebp';
+export { default as pcmToWav } from './pcmToWav';
+export { default as svgrToFile } from './svgrToFile';
